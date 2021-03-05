be5b36bbeca406534b3ea8c33606f56e6d066934
rana-abhishek <rana@nayantech.com>
2021-03-05 14:48:51 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const grandparentClickHandler = document
+  .querySelector("#grandparent")
+  .addEventListener("click", () => {
+    console.log("Grandparent input clicked");
+  });
+
+const parentClickHandler = document.querySelector("#parent").addEventListener(
+  "click",
+  () => {
+    console.log("parent input clicked");
+  },
+  true
+);
+
+const childClickHandler = document
+  .querySelector("#child")
+  .addEventListener("click", () => {
+    console.log("Child input clicked");
+  });
